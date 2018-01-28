388ca398b0a69c6a8ca0dc886154f5f99d422f2f
Timo Schindler <timo.schindler@oth-regensburg.de>
2018-01-28 17:33:29 +0100

    fixed error with a returned null in balance

--- repository/sql/transactions.go
+++ repository/sql/transactions.go
@@ -12,7 +12,7 @@ func (m *Mysql) GetUsersWithBalance(ctx context.Context) ([]*contract.User, erro
 	logger.Debug("enter repo")
 
 	users := []*contract.User{}
-	err := m.db.Select(&users, "SELECT users.user_id, users.email, users.name, users.is_admin, SUM(transactions.value) AS 'balance' FROM users LEFT JOIN transactions ON users.user_id = transactions.user_id WHERE users.deleted_at IS NULL GROUP BY users.user_id")
+	err := m.db.Select(&users, "SELECT users.user_id, users.email, users.name, users.is_admin, COALESCE(SUM(transactions.value), 0.00) AS 'balance' FROM users LEFT JOIN transactions ON users.user_id = transactions.user_id WHERE users.deleted_at IS NULL GROUP BY users.user_id")
 	if err != nil {
 		logger.WithError(err).Error("failed to fetch users from db")
 		return nil, errors.InternalServerError("db error", err)
