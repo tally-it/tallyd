4ef2917a3bada3ab8299adf540c65c2da4c61708
Timo Schindler <timo.schindler@oth-regensburg.de>
2018-02-01 17:48:44 +0100

    minor changes

--- contract/user.go
+++ contract/user.go
@@ -2,26 +2,25 @@ package contract
 
 import "github.com/jmoiron/sqlx/types"
 
-
 // swagger:model
 type UserSlice []User
 
 // swagger:model
 type User struct {
-	UserID   int           `json:"userID" db:"user_id"`
-	Name     string        `json:"name" db:"name"`
-	Email    string        `json:"email" db:"email"`
-	IsActive bool          `json:"active"`
-	IsAdmin  types.BitBool `json:"isAdmin" db:"is_admin"`
-	Balance  float64       `json:"balance" db:"balance"`
+	UserID    int           `json:"userID" db:"user_id"`
+	Name      string        `json:"name" db:"name"`
+	Email     string        `json:"email" db:"email"`
+	IsBlocked types.BitBool `json:"isBlocked" db:"is_blocked"`
+	IsAdmin   types.BitBool `json:"isAdmin" db:"is_admin"`
+	Balance   float64       `json:"balance" db:"balance"`
 }
 
 // swagger:model
 type AddUserRequestBody struct {
 	// required: true
-	Name     string `json:"name"`
+	Name string `json:"name"`
 	// required: true
-	Email    string `json:"email"`
+	Email string `json:"email"`
 	// required: true
 	Password string `json:"password"`
 }
