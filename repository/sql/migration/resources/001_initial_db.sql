db0a8da0d724604dfdc110a2d52e23f0855ed578
Lars Hemala <lars@hemala.de>
2018-02-08 16:23:42 +0100

    fix db migration

--- repository/sql/migration/resources/001_initial_db.sql
+++ repository/sql/migration/resources/001_initial_db.sql
@@ -1,50 +1,40 @@
 -- +migrate Up
-
-CREATE TABLE IF NOT EXISTS categories (
-  category_id INT(11)    NOT NULL  AUTO_INCREMENT,
-  name        INT(11)    NOT NULL,
-  visible     TINYINT(1) NOT NULL  DEFAULT '1',
-  active      TINYINT(1) NOT NULL  DEFAULT '0',
-  is_root     TINYINT(1) NOT NULL  DEFAULT '1',
-  PRIMARY KEY (category_id)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
-
-CREATE TABLE IF NOT EXISTS category_parent_map (
-  cateogry_id        INT(11) NOT NULL  AUTO_INCREMENT,
-  parent_category_id INT(11) NOT NULL,
-  PRIMARY KEY (cateogry_id, parent_category_id)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
-
-CREATE TABLE transactions
+CREATE TABLE categories
 (
-  transaction_id INT AUTO_INCREMENT
+  category_id INT AUTO_INCREMENT
     PRIMARY KEY,
-  user_id        INT                                 NULL,
-  SKU_id         INT                                 NULL,
-  value          DECIMAL(15, 2)                      NOT NULL,
-  tag            VARCHAR(255)                        NULL,
-  added_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at     TIMESTAMP                           NULL,
-  CONSTRAINT fk_transactions_users
-  FOREIGN KEY (user_id) REFERENCES users (user_id)
-    ON UPDATE CASCADE
-    ON DELETE SET NULL,
-  CONSTRAINT `transactions__product.SKU_id_fk`
-  FOREIGN KEY (SKU_id) REFERENCES products (SKU_id)
-    ON UPDATE CASCADE
+  name        INT                    NOT NULL,
+  visible     TINYINT(1) DEFAULT '1' NOT NULL,
+  active      TINYINT(1) DEFAULT '0' NOT NULL,
+  is_root     TINYINT(1) DEFAULT '1' NOT NULL
 )
   ENGINE = InnoDB
   CHARSET = utf8mb4;
 
-CREATE INDEX user_id
-  ON transactions (user_id);
+CREATE TABLE category_parent_map
+(
+  cateogry_id        INT AUTO_INCREMENT,
+  parent_category_id INT NOT NULL,
+  PRIMARY KEY (cateogry_id, parent_category_id)
+)
+  ENGINE = InnoDB
+  CHARSET = utf8mb4;
 
-CREATE INDEX `transactions__product.SKU_id_fk`
-  ON transactions (SKU_id);
+CREATE TABLE product_category_map
+(
+  product_id  INT AUTO_INCREMENT,
+  category_id INT NOT NULL,
+  PRIMARY KEY (product_id, category_id),
+  CONSTRAINT product_category_map_ibfk_1
+  FOREIGN KEY (category_id) REFERENCES categories (category_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+)
+  ENGINE = InnoDB
+  CHARSET = utf8mb4;
+
+CREATE INDEX product_category_map_ibfk_1
+  ON product_category_map (category_id);
 
 CREATE TABLE products
 (
@@ -66,14 +56,11 @@ CREATE TABLE products
 CREATE INDEX products_SKU_index
   ON products (SKU_id);
 
-
-CREATE TABLE IF NOT EXISTS product_category_map (
-  product_id  INT(11) NOT NULL  AUTO_INCREMENT,
-  category_id INT(11) NOT NULL,
-  PRIMARY KEY (product_id, category_id)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+ALTER TABLE product_category_map
+  ADD CONSTRAINT product_category_map_ibfk_2
+FOREIGN KEY (product_id) REFERENCES products (product_id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
 
 CREATE TABLE stock
 (
@@ -86,11 +73,7 @@ CREATE TABLE stock
   CONSTRAINT stock_products_SKU_fk
   FOREIGN KEY (SKU_id) REFERENCES products (SKU_id)
     ON UPDATE CASCADE
-    ON DELETE CASCADE,
-  CONSTRAINT stock_ibfk_1
-  FOREIGN KEY (user_id) REFERENCES users (user_id)
-    ON UPDATE SET NULL
-    ON DELETE SET NULL
+    ON DELETE CASCADE
 )
   ENGINE = InnoDB
   CHARSET = utf8mb4;
@@ -100,6 +83,44 @@ CREATE INDEX stock_products_SKU_fk
 
 CREATE INDEX stock_ibfk_1
   ON stock (user_id);
+
+CREATE TABLE transactions
+(
+  transaction_id INT AUTO_INCREMENT
+    PRIMARY KEY,
+  user_id        INT                                 NULL,
+  SKU_id         INT                                 NULL,
+  value          DECIMAL(15, 2)                      NOT NULL,
+  tag            VARCHAR(255)                        NULL,
+  added_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at     TIMESTAMP                           NULL,
+  CONSTRAINT `transactions__product.SKU_id_fk`
+  FOREIGN KEY (SKU_id) REFERENCES products (SKU_id)
+    ON UPDATE CASCADE
+    ON DELETE SET NULL
+)
+  ENGINE = InnoDB
+  CHARSET = utf8mb4;
+
+CREATE INDEX user_id
+  ON transactions (user_id);
+
+CREATE INDEX `transactions__product.SKU_id_fk`
+  ON transactions (SKU_id);
+
+CREATE TABLE user_auths
+(
+  user_auth_id INT AUTO_INCREMENT
+    PRIMARY KEY,
+  user_id      INT          NOT NULL,
+  method       VARCHAR(255) NOT NULL,
+  value        TINYBLOB     NULL
+)
+  ENGINE = InnoDB
+  CHARSET = utf8mb4;
+
+CREATE INDEX fk_user_auths_user
+  ON user_auths (user_id);
 
 CREATE TABLE users
 (
@@ -117,38 +138,20 @@ CREATE TABLE users
   ENGINE = InnoDB
   CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS user_auths (
-  user_auth_id INT(11)      NOT NULL  AUTO_INCREMENT,
-  user_id      INT(11)      NOT NULL,
-  method       VARCHAR(255) NOT NULL,
-  value        TINYBLOB,
-  PRIMARY KEY (user_auth_id)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+ALTER TABLE stock
+  ADD CONSTRAINT stock_ibfk_1
+FOREIGN KEY (user_id) REFERENCES users (user_id)
+  ON UPDATE SET NULL
+  ON DELETE SET NULL;
 
 ALTER TABLE transactions
-  ADD CONSTRAINT fk_transactions_users FOREIGN KEY (user_id) REFERENCES users (user_id)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE product_category_map
-  ADD CONSTRAINT product_category_map_ibfk_1 FOREIGN KEY (category_id) REFERENCES categories (category_id)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE,
-  ADD CONSTRAINT product_category_map_ibfk_2 FOREIGN KEY (product_id) REFERENCES products (product_id)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE;
-
-ALTER TABLE stock
-  ADD CONSTRAINT stock_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (user_id)
-  ON DELETE SET NULL
-  ON UPDATE SET NULL,
-  ADD CONSTRAINT stock_ibfk_2 FOREIGN KEY (SKU_id) REFERENCES products (product_id)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE;
+  ADD CONSTRAINT fk_transactions_users
+FOREIGN KEY (user_id) REFERENCES users (user_id)
+  ON UPDATE CASCADE
+  ON DELETE SET NULL;
 
 ALTER TABLE user_auths
-  ADD CONSTRAINT fk_user_auths_user FOREIGN KEY (user_id) REFERENCES users (user_id)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE;
+  ADD CONSTRAINT fk_user_auths_user
+FOREIGN KEY (user_id) REFERENCES users (user_id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
