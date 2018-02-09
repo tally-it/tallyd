e47f25f1447d15c691aa59901ba3225565751612
Lars Hemala <lars@hemala.de>
2018-02-09 13:21:29 +0100

    improve simple testcase

--- sit/sit_test.go
+++ sit/sit_test.go
@@ -3,20 +3,18 @@ package sit
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"testing"
 	"time"
 
 	"github.com/marove2000/hack-and-pay/contract"
 
-	"fmt"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/require"
-	"io/ioutil"
 )
 
-func TestStuff(t *testing.T) {
+func TestSimple(t *testing.T) {
 	db := NewDB(t)
 	db.Clear()
 
@@ -29,22 +27,25 @@ func TestStuff(t *testing.T) {
 	token := userLogin(t, testuser, testpass)
 	require.NotZero(t, token)
 
-	user := userGetById(t, uid)
+	user := userGetById(t, token, uid)
 	require.Zero(t, user.Balance.Cmp(decimal.Zero))
 
-	spew.Dump(user)
+	transactionAdd(t, token, uid, 15.33)
+
+	user = userGetById(t, token, uid)
+	require.Zero(t, user.Balance.Cmp(decimal.NewFromFloat(15.33)))
+
+	user = userGetById(t, "", uid)
+	require.Zero(t, user.Balance.Cmp(decimal.Zero))
 }
 
 func userCreate(t *testing.T, name, pass string) int {
-	user := &contract.AddUserRequestBody{
-		Name:     name,
-		Password: pass,
-	}
-
-	b, err := json.Marshal(user)
-	require.NoError(t, err)
-
-	r, err := http.NewRequest(http.MethodPost, "http://127.0.0.1:8080/v1/user", bytes.NewBuffer(b))
+	r, err := http.NewRequest(http.MethodPost, "http://127.0.0.1:8080/v1/user", bytes.NewBufferString(`
+{	
+	"name":"`+name+`",
+	"password":"`+pass+`"
+}
+`))
 	require.NoError(t, err)
 
 	c := http.Client{
@@ -64,15 +65,12 @@ func userCreate(t *testing.T, name, pass string) int {
 }
 
 func userLogin(t *testing.T, name, pass string) string {
-	user := &contract.LoginRequestBody{
-		Name:     name,
-		Password: pass,
-	}
-
-	b, err := json.Marshal(user)
-	require.NoError(t, err)
-
-	r, err := http.NewRequest(http.MethodPost, "http://127.0.0.1:8080/v1/login", bytes.NewBuffer(b))
+	r, err := http.NewRequest(http.MethodPost, "http://127.0.0.1:8080/v1/login", bytes.NewBufferString(`
+{	
+	"name":"`+name+`",
+	"password":"`+pass+`"
+}
+`))
 	require.NoError(t, err)
 
 	c := http.Client{
@@ -84,15 +82,20 @@ func userLogin(t *testing.T, name, pass string) string {
 	defer rsp.Body.Close()
 	require.Equal(t, http.StatusOK, rsp.StatusCode)
 
-	token, err := ioutil.ReadAll(rsp.Body)
+	var rspData contract.LoginResponse
+	err = json.NewDecoder(rsp.Body).Decode(&rspData)
 	require.NoError(t, err)
 
-	return string(token)
+	return rspData.JWT
 }
 
-func userGetById(t *testing.T, userID int) *contract.User {
+func userGetById(t *testing.T, token string, userID int) *contract.User {
 	r, err := http.NewRequest(http.MethodGet, "http://127.0.0.1:8080/v1/user/"+fmt.Sprint(userID), nil)
 	require.NoError(t, err)
+
+	if token != "" {
+		r.Header.Set("Authorization", "Bearer: "+token)
+	}
 
 	c := http.Client{
 		Timeout: time.Second * 5,
@@ -108,4 +111,24 @@ func userGetById(t *testing.T, userID int) *contract.User {
 	require.NoError(t, err)
 
 	return &rData
+}
+
+func transactionAdd(t *testing.T, token string, userId int, value float64) {
+	r, err := http.NewRequest(http.MethodPost, "http://127.0.0.1:8080/v1/user/"+fmt.Sprint(userId)+"/transaction", bytes.NewBufferString(`
+{	
+	"userID":`+fmt.Sprint(userId)+`,
+	"value":`+fmt.Sprint(value)+`
+}
+`))
+	r.Header.Set("Authorization", "Bearer: "+token)
+
+	require.NoError(t, err)
+
+	c := http.Client{
+		Timeout: time.Second * 5,
+	}
+	rsp, err := c.Do(r)
+	require.NoError(t, err)
+	defer rsp.Body.Close()
+	require.Equal(t, http.StatusNoContent, rsp.StatusCode)
 }
